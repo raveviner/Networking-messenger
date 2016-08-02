6ac4e75f19b3d8e0dc8084ca4533f0768ad71f79
user <user@Ubuntu-VM>
2016-08-02 16:45:07 +0300

    # WARNING: head commit changed in the meantime
    init push

---
+++ SocketUtils/src/UDPSocket.h
@@ -0,0 +1,28 @@
+
+
+#ifndef UDPSOCKET_H_
+#define UDPSOCKET_H_
+
+#include <netinet/in.h>
+#include <inttypes.h>
+#include <strings.h>
+#include <stdio.h>
+#include <iostream>
+#include "Socket.h"
+
+using namespace std;
+namespace npl{
+
+class UDPSocket: public Socket {
+
+public:
+	UDPSocket(int port);
+	UDPSocket();
+	int recv(char* buffer, int length);
+	int sendTo(const string& msg, const string& ip, int port);
+	int reply(const string& msg);
+	void close();
+
+};
+}
+#endif /* UDPSOCKET_H_ */
